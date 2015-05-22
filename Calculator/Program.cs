3e6675bc18427bb42091e64359141394b50da81b
Jefferin Joseph <jeff3754@gmail.com>
2015-05-22 12:36:40 +0530

    Type Changes
    switch replaced with if

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,33 +8,37 @@ namespace Calculator
 {
     class Program
     {
-        
         static void Main(string[] args)
         {
             while (true)
             {
                 try
                 {
-                    Calculation calculation = new Calculation();
-                    Selection selection = new Selection();
+                    var calculation = new Calculation();
+                    var selection = new Selection();
                     ICalculator icalculator;
+
                     Console.WriteLine("----Welcome to the Magic calculator----");
                     icalculator = selection.SelectCalculatorMethod();
-                    int operation = selection.SelectOperationMethod();
+
+                    var operation = selection.SelectOperationMethod();
                     Console.Write("Enter the First number :");
-                    double firstnumber = double.Parse(Console.ReadLine());
+
+                    var firstnumber = double.Parse(Console.ReadLine());
                     Console.Write("Enter the Second number :");
-                    double secondnumber = double.Parse(Console.ReadLine());
-                    double result = calculation.CalculationMethod(icalculator, operation, firstnumber, secondnumber);
+
+                    var secondnumber = double.Parse(Console.ReadLine());
+                    var result = calculation.CalculationMethod(icalculator, operation, firstnumber, secondnumber);
                     icalculator.SaveResult(result);
+
                     Queue<double> m= icalculator.ViewResult();
+
                     Console.WriteLine("The Result is {0}", result);
                     Console.WriteLine("press y to view previous results");
-                    switch(Console.ReadLine())
+
+                    if (Console.ReadLine() == "y")
                     {
-                        case "y": foreach (double j in m)
-                                Console.WriteLine(j);
-                            break;
+                        foreach (double j in m) Console.WriteLine(j);
                     }
                 }
                 catch
